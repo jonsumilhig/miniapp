805a420ce86435f41397db2510ce531db56aa8ae
aljon sumilhig <aljonsumilhig@gmail.com>
2020-10-16 09:12:52 +0800

    add bootstrap scaffolding, admin-lte and @fullhuman/postcss-purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,27 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+
+let sass = mix.sass('resources/sass/app.scss', 'public/css')
+
+if (mix.inProduction()) {
+    sass.options({
+        postCss: [
+            require('@fullhuman/postcss-purgecss')({
+                content: [
+                    './node_modules/bootstrap/dist/js/**/*.js',
+                    './node_modules/admin-lte/build/js/**/*.js',
+                    './resources/js/**/*.js',
+                    './resources/views/**/*.blade.php'
+                ]
+            })
+        ]
+    });
+}
+
